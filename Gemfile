09c12a504c57a1d31301901c6b25fc477e2785a1
Lucas Pacheco <xekonha@gmail.com>
2021-11-05 15:37:43 -0300

    XLS Importer

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'devise'
 gem 'devise-i18n'
+gem "xsv"
 
 gem 'autoprefixer-rails', '10.2.5'
 gem 'font-awesome-sass'
--- app/models/nota.rb
+++ app/models/nota.rb
@@ -2,8 +2,6 @@ class Nota < ApplicationRecord
   belongs_to :pedido
   has_many :items_notas
   validates :emissao, :cpf_destinatario, :emitente, :numero_nota, :chave, presence: true
-  validates :cpf_destinatario, cpf: { message: 'CPF válido' }, length: { in: 11..14 }
-  validates :chave, numericality: true, length: { is: 44 }, uniqueness: true
-  validates :data_pedido, :data_resposta, :periodo_inicial, :periodo_final, inclusion: { in: (Date.today-2.years..Date.today) }
-  validates :periodo_final inclusion: { in: :periodo_inicial..Date.today }
+  validates :cpf_destinatario, length: { in: 11..14 }
+  validates :chave, numericality: true, length: { is: 44 }
 end
