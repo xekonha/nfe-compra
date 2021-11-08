9568d19be8e3e8f9b9404f3d51e63cc7added0be
Lucas Pacheco <xekonha@gmail.com>
2021-11-08 18:13:02 -0300

    Dsgov com devise funcionando

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,7 +26,7 @@ require("channels")
 // import "bootstrap";
 
 // gov.br
-import '@govbr/dsgov/dist/dsgov';
+import "@govbr/dsgov/dist/dsgov";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
