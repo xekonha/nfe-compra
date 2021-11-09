aacc53ad4ebac5d64c066749d1ea34d0faf40e85
Mario Bais <mariocfbais@gmail.com>
2021-11-09 11:36:44 -0300

    2 teste senha automatica

--- app/controllers/notas_controller.rb
+++ app/controllers/notas_controller.rb
@@ -2,8 +2,4 @@ class NotasController < ApplicationController
   def index
     @notas = Notas.where("cpf_destinatario = 'current_user'")
   end
-
-  # def periodo
-  #   @notas_periodo = Notas.where(cpf_destinatario = current_user).and(emissao >= periodo_inicial).and(emissao <= periodo_final) # período inicial e final informados no view index
-  # end
 end
