aa8b7ca51b522250bc6a925d6f62bf0e953cee1f
Mario Bais <mariocfbais@gmail.com>
2021-11-10 17:25:21 -0300

    Heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,122 +1,122 @@
-# @cpfs = RepoNota.select(:cpf_destinatario).distinct
 
 
-# /home/mariocfbais/code/MarioCesarBais/nfe-compra/tmp/Nota Fiscal cpf 0464.xlsx
-  # Registers Users
-    # @chave = 1000000000
-    # @chaves = {}
-    # @users = User.all
-    # x = Xsv::Workbook.open(@file_path)
-    # sheet = x.sheets[0]
-    # sheet.row_skip = 1
-    # sheet.each_row do |row|
-    #   # Dados da nota sem mercadorias (itens)
-    #   nota = RepoNota.new
-    #   nota.emissao = row[0]
-    #   if nota.emissao.nil?
-    #     break
-    #   end
-    #   nota.cpf_destinatario = row[1].gsub(/\D/, '')
-    #   @user_now = @users[rand(0...@users.length)]
-    #   nota.nome_destinatario = row[2]
-    #   nota.emitente = row[3]
-    #   nota.nome_emitente = row[4]
-    #   nota.descricao_cfop = row[5]
-    #   nota.numero_nota = row[6].to_i
-    #   chave_tmp = row[7]
-    #   nota.chave = chave_tmp.gsub(/\s/, '')
-    #   if @chaves.keys.include?(nota.chave)
-    #     nota = RepoNota.where("chave = '#{@chaves[nota.chave]}'")[0]
-    #   else
-    #     nota.cpf_destinatario = @user_now.cpf
-    #     nota.nome_destinatario = @user_now.name
-    #     @chave += 10
-    #     @chave_simulada = @chave.to_s.rjust(44, "0")
-    #     @chaves[nota.chave] = @chave_simulada
-    #     nota.chave = @chave_simulada
-    #     nota.save!
-    #   end
-    #   # Dados das mercadorias (itens da NFe)
-    #   item = RepoItemNota.new
-    #   item.repo_nota = nota
-    #   item.descricao = row[8]
-    #   item.unidade_comercial = row[9]
-    #   item.quantidade_comercial = row[10]
-    #   item.quantidade_comercial = item.quantidade_comercial.to_f
-    #   item.quantidade_comercial = (item.quantidade_comercial * rand(1..3)).truncate(2)
-    #   item.valor_unitario = row[11].to_f
-    #   item.valor_unitario = (item.valor_unitario * rand(1.0..3.0)).truncate(2)
-    #   item.valor_total = (item.valor_unitario * item.quantidade_comercial).truncate(2)
-    #   item.save!
-    # end
 
-    # NOT registers users
-    # "/home/rainey/code/PaxecoL/nfe-compra/tmp/Nota Fiscal cpf xxxx.xlsx"
-    @file_path = "db/Nota Fiscal cpf 0464.xlsx"
-    @chave = 0
-    @chaves = {}
-    cpfs = {}
-    nomes = {}
-    x = Xsv::Workbook.open(@file_path)
-    sheet = x.sheets[0]
-    sheet.row_skip = 1
-    sheet.each_row do |row|
-      # Dados da nota sem mercadorias (itens)
-      nota = RepoNota.new
-      nota.emissao = row[0]
-      if nota.emissao.nil?
-        break
-      end
-      nota.cpf_destinatario = row[1].gsub(/\D/, '')
-      nota.nome_destinatario = row[2]
-      # Faker::Name.first_name       #=> "Kaci"
-      # Faker::Name.middle_name      #=> "Abraham"
-      # Faker::Name.last_name        #=> "Ernser"
-      if cpfs.keys.include?(nota.cpf_destinatario)
-        nota.nome_destinatario = nomes[nota.cpf_destinatario]
-        nota.cpf_destinatario = cpfs[nota.cpf_destinatario]
-      else
-        cpfs[nota.cpf_destinatario] = Faker::CPF.numeric
-        nomes[nota.cpf_destinatario] = "#{Faker::Name.first_name} #{Faker::Name.middle_name} #{Faker::Name.last_name}"
-        nota.nome_destinatario = nomes[nota.cpf_destinatario]
-        nota.cpf_destinatario = cpfs[nota.cpf_destinatario]
-      end
-      nota.emitente = row[3]
-      nota.nome_emitente = row[4]
-      nota.descricao_cfop = row[5]
-      nota.numero_nota = row[6].to_i
-      chave_tmp = row[7]
-      nota.chave = chave_tmp.gsub(/\s/, '')
-      if @chaves.keys.include?(nota.chave)
-        nota = RepoNota.where("chave = '#{@chaves[nota.chave]}'")[0]
-      else
-        @chave += 10
-        @chave_simulada = @chave.to_s.rjust(44, "0")
-        @chaves[nota.chave] = @chave_simulada
-        nota.chave = @chave_simulada
-        nota.save!
-      end
-      # Dados das mercadorias (itens da NFe)
-      item = RepoItemNota.new
-      item.repo_nota = nota
-      item.descricao = row[8]
-      item.unidade_comercial = row[9]
-      item.unidade_comercial = '-' if item.unidade_comercial.blank?
-      item.quantidade_comercial = row[10]
-      item.quantidade_comercial = item.quantidade_comercial.to_f
-      item.quantidade_comercial = (item.quantidade_comercial * rand(1..3)).truncate(2)
-      item.valor_unitario = row[11].to_f
-      item.valor_unitario = (item.valor_unitario * rand(1.0..3.0)).truncate(2)
-      item.valor_total = (item.valor_unitario * item.quantidade_comercial).truncate(2)
-      item.save!
-    end
+# # /home/mariocfbais/code/MarioCesarBais/nfe-compra/tmp/Nota Fiscal cpf 0464.xlsx
+#   # Registers Users
+#     # @chave = 1000000000
+#     # @chaves = {}
+#     # @users = User.all
+#     # x = Xsv::Workbook.open(@file_path)
+#     # sheet = x.sheets[0]
+#     # sheet.row_skip = 1
+#     # sheet.each_row do |row|
+#     #   # Dados da nota sem mercadorias (itens)
+#     #   nota = RepoNota.new
+#     #   nota.emissao = row[0]
+#     #   if nota.emissao.nil?
+#     #     break
+#     #   end
+#     #   nota.cpf_destinatario = row[1].gsub(/\D/, '')
+#     #   @user_now = @users[rand(0...@users.length)]
+#     #   nota.nome_destinatario = row[2]
+#     #   nota.emitente = row[3]
+#     #   nota.nome_emitente = row[4]
+#     #   nota.descricao_cfop = row[5]
+#     #   nota.numero_nota = row[6].to_i
+#     #   chave_tmp = row[7]
+#     #   nota.chave = chave_tmp.gsub(/\s/, '')
+#     #   if @chaves.keys.include?(nota.chave)
+#     #     nota = RepoNota.where("chave = '#{@chaves[nota.chave]}'")[0]
+#     #   else
+#     #     nota.cpf_destinatario = @user_now.cpf
+#     #     nota.nome_destinatario = @user_now.name
+#     #     @chave += 10
+#     #     @chave_simulada = @chave.to_s.rjust(44, "0")
+#     #     @chaves[nota.chave] = @chave_simulada
+#     #     nota.chave = @chave_simulada
+#     #     nota.save!
+#     #   end
+#     #   # Dados das mercadorias (itens da NFe)
+#     #   item = RepoItemNota.new
+#     #   item.repo_nota = nota
+#     #   item.descricao = row[8]
+#     #   item.unidade_comercial = row[9]
+#     #   item.quantidade_comercial = row[10]
+#     #   item.quantidade_comercial = item.quantidade_comercial.to_f
+#     #   item.quantidade_comercial = (item.quantidade_comercial * rand(1..3)).truncate(2)
+#     #   item.valor_unitario = row[11].to_f
+#     #   item.valor_unitario = (item.valor_unitario * rand(1.0..3.0)).truncate(2)
+#     #   item.valor_total = (item.valor_unitario * item.quantidade_comercial).truncate(2)
+#     #   item.save!
+#     # end
 
+#     # NOT registers users
+#     # "/home/rainey/code/PaxecoL/nfe-compra/tmp/Nota Fiscal cpf xxxx.xlsx"
+#     @file_path = "db/Nota Fiscal cpf 0464.xlsx"
+#     @chave = 0
+#     @chaves = {}
+#     cpfs = {}
+#     nomes = {}
+#     x = Xsv::Workbook.open(@file_path)
+#     sheet = x.sheets[0]
+#     sheet.row_skip = 1
+#     sheet.each_row do |row|
+#       # Dados da nota sem mercadorias (itens)
+#       nota = RepoNota.new
+#       nota.emissao = row[0]
+#       if nota.emissao.nil?
+#         break
+#       end
+#       nota.cpf_destinatario = row[1].gsub(/\D/, '')
+#       nota.nome_destinatario = row[2]
+#       # Faker::Name.first_name       #=> "Kaci"
+#       # Faker::Name.middle_name      #=> "Abraham"
+#       # Faker::Name.last_name        #=> "Ernser"
+#       if cpfs.keys.include?(nota.cpf_destinatario)
+#         nota.nome_destinatario = nomes[nota.cpf_destinatario]
+#         nota.cpf_destinatario = cpfs[nota.cpf_destinatario]
+#       else
+#         cpfs[nota.cpf_destinatario] = Faker::CPF.numeric
+#         nomes[nota.cpf_destinatario] = "#{Faker::Name.first_name} #{Faker::Name.middle_name} #{Faker::Name.last_name}"
+#         nota.nome_destinatario = nomes[nota.cpf_destinatario]
+#         nota.cpf_destinatario = cpfs[nota.cpf_destinatario]
+#       end
+#       nota.emitente = row[3]
+#       nota.nome_emitente = row[4]
+#       nota.descricao_cfop = row[5]
+#       nota.numero_nota = row[6].to_i
+#       chave_tmp = row[7]
+#       nota.chave = chave_tmp.gsub(/\s/, '')
+#       if @chaves.keys.include?(nota.chave)
+#         nota = RepoNota.where("chave = '#{@chaves[nota.chave]}'")[0]
+#       else
+#         @chave += 10
+#         @chave_simulada = @chave.to_s.rjust(44, "0")
+#         @chaves[nota.chave] = @chave_simulada
+#         nota.chave = @chave_simulada
+#         nota.save!
+#       end
+#       # Dados das mercadorias (itens da NFe)
+#       item = RepoItemNota.new
+#       item.repo_nota = nota
+#       item.descricao = row[8]
+#       item.unidade_comercial = row[9]
+#       item.unidade_comercial = '-' if item.unidade_comercial.blank?
+#       item.quantidade_comercial = row[10]
+#       item.quantidade_comercial = item.quantidade_comercial.to_f
+#       item.quantidade_comercial = (item.quantidade_comercial * rand(1..3)).truncate(2)
+#       item.valor_unitario = row[11].to_f
+#       item.valor_unitario = (item.valor_unitario * rand(1.0..3.0)).truncate(2)
+#       item.valor_total = (item.valor_unitario * item.quantidade_comercial).truncate(2)
+#       item.save!
+#     end
+@cpfs = RepoNota.select(:cpf_destinatario).distinct
 10.times {
   @user = User.new
   @user.email = Faker::Internet.email
   @user.password = '123456'
   @cpf = @cpfs.sample
-  @user.cpf = @cpf
+  @user.cpf = @cpf.cpf_destinatario
   @cpfs.where(cpf_destinatario: @cpf.cpf_destinatario).destroy_all
   @user.name = Faker::Name.name
   @user.save!
@@ -131,7 +131,7 @@ User.all.each { |user|
     @pedido.data_pedido = rand(@pedido.periodo_final..Date.today)
     @pedido.situacao = 'pendente'
     @pedido.data_resposta = nil
-    @pedido.save!
+    @pedido.save
   }
 }
 puts 'criados pedidos'
