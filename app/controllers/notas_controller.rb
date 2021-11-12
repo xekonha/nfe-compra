93db376c193a9fb674d7523b4f058f751e835c50
Lucas Pacheco <xekonha@gmail.com>
2021-11-12 10:09:54 -0300

    Nota View

--- app/controllers/notas_controller.rb
+++ app/controllers/notas_controller.rb
@@ -1,6 +1,19 @@
 class NotasController < ApplicationController
   # before_action :authenticate_user!
   def index
-    @notas = Nota.where(cpf_destinatario: current_user.cpf) # .and(restingir período)
+    @notas = Nota.where(cpf_destinatario: current_user.cpf)
+    @totais = totalizar(@notas);
+    puts "TOTAIS: #{@totais}"
+  end
+
+  private
+
+  def totalizar(notas)
+    @total = {}
+    notas.each_with_index do |nota, index|
+      itens = ItemNota.where(nota: nota)
+      @total[nota.id] = itens.sum(&:valor_total)
+    end
+    @total
   end
 end
